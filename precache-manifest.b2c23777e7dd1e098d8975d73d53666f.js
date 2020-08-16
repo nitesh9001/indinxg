9ac470b5c6dca9ce22aafc6bd0fdba023164d63f
inter <intern@cybersrcc.com>
2020-08-16 21:32:56 +0530

    Updates

---
+++ precache-manifest.b2c23777e7dd1e098d8975d73d53666f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe184d94c9fec399b671c4aadec7e23",
+    "url": "/indinxg/index.html"
+  },
+  {
+    "revision": "df8987754419eb9bb783",
+    "url": "/indinxg/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7aebd7e5fca1f81116cb",
+    "url": "/indinxg/static/js/2.cba61e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/indinxg/static/js/2.cba61e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8987754419eb9bb783",
+    "url": "/indinxg/static/js/main.048b2184.chunk.js"
+  },
+  {
+    "revision": "63ef7b7d26bb149759df",
+    "url": "/indinxg/static/js/runtime-main.f50241af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/indinxg/static/media/logo.5d5d9eef.svg"
+  }
+]);
